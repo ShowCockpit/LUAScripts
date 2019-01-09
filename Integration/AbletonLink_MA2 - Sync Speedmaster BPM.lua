213d4d051ebfb837f3165e9ae600f10d562fb3b5
Ricardo Dias <ricdays@gmail.com>
2019-01-09 23:21:43 +0000

    Update AbletonLink_MA2 - Sync Speedmaster BPM.lua

--- Integration/AbletonLink_MA2 - Sync Speedmaster BPM.lua
+++ Integration/AbletonLink_MA2 - Sync Speedmaster BPM.lua
@@ -20,10 +20,20 @@ while true do
     bpm = round(bpm, 2)
     
     if bpm ~= lastBpm then
+        -- Account for phase
+    	phase = AbletonLink.GetPhase()
+    	intPhase = math.floor(phase)
+    	subPhase = phase - intPhase;
+        remPhase = 1.0 - subPhase;
+        beatTime = (60 / bpm) * 1000;
+        waitTimeMS = math.floor(remPhase * beatTime);
+        
+        sleep(waitTimeMS / 1000)
+    
     	GrandMA2.CommandExecute_ButtonClick('SpecialMaster 3.1 At ' .. bpm)
     	print(bpm)
     	lastBpm = bpm
     end
 	
-	sleep(0.01)
+    sleep(0.05)
 end
